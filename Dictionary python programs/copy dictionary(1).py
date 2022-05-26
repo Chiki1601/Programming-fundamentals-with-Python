614b99d838c426fa5bc9f38ccde34fdd4ef99899
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:52:41 +0530

    Create copy dictionary(1).py

---
+++ Dictionary python programs/copy dictionary(1).py
@@ -0,0 +1,17 @@
+#copy dictionary
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+copydict = bike.copy()
+
+print("Dictionary data: ")
+print(copydict)
