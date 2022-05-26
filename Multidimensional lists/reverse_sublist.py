70310c503be06e8aa260424c6096a81d5aea1ab7
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:59:50 +0530

    Create reverse_sublist.py

---
+++ Multidimensional lists/reverse_sublist.py
@@ -0,0 +1,15 @@
+# reverse() method "Reverse sublist " in multidimentional list
+
+
+
+mList = [[1,2,3,4,5],[1,2,3,4,5,6],[1,2,3,4,5,6,7]]
+
+
+
+print("Multidimentional list is given by : ",mList)
+
+mList[0].reverse()
+mList[1].reverse()
+mList[2].reverse()
+
+print("\n\nMultidimentional list is given by(After reverse) \n : ",mList)
