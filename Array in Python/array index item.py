688fbb8f4ad857700e15b89d92030f27ee7f01d5
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 23:01:08 +0530

    Create array index item.py

---
+++ Array in Python/array index item.py
@@ -0,0 +1,8 @@
+#array index item
+
+import array as arr   #0 = 10   1 = 20      -1 = 50   -2 = 40
+numArr = arr.array('i',[10,20,30,40,50])
+
+print("Array item at index 3 is: ")
+
+print(numArr[-1])  #40
