b31479dd53d0effdc6123616c1a36b290f3c963c
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 19:00:21 +0530

    Create extend().py

---
+++ Multidimensional lists/extend().py
@@ -0,0 +1,10 @@
+# extend() method in multidimentional list
+
+
+mList = [[1,2,3,4,5],[1,2,3,4,5,6],[1,2,3,4,5,6,7]]
+
+print("Multidimentional list is given by : ",mList)
+
+mList[0].extend([16,28,99,198])
+
+print("\n\nMultidimentional list is given by(After extend) \n : ",mList)
