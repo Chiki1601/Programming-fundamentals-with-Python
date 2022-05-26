509ffce93492e88480e86b6d4a272b7e613cc65a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:58:07 +0530

    Create Create&Access.py

---
+++ Dictionary python programs/Create&Access.py
@@ -0,0 +1,15 @@
+#dictionay program in Python
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+
+print("Dicitonary data is given by: ",bike)
