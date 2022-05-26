7b0f782ec98e6f69c1540e35f8a0786cdc1aa359
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:57:02 +0530

    Create items().py

---
+++ Dictionary python programs/items().py
@@ -0,0 +1,17 @@
+#itemes() method in python dictionary
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+print("Dicitonary data is given by: ")
+
+for x in bike.items():
+    print(x)
