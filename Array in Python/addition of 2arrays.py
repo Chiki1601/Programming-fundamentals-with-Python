72d680944e85c2a3b7c28a0bf6bce3d04d65cfe8
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 22:58:38 +0530

    Create addition of 2arrays.py

---
+++ Array in Python/addition of 2arrays.py
@@ -0,0 +1,12 @@
+#how to add two arrays
+
+import array as arr
+
+numA = arr.array('i',[10,20,30])
+numB = arr.array('i',[100,200,300])
+
+numC = numA + numB
+
+print("Array A is: ",numA)
+print("Array B is: ",numB)
+print("Array C is: ",numC)
