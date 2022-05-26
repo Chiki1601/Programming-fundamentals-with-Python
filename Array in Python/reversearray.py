682133b3f66288792cdd3a42301af6ebca656e3b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 22:59:46 +0530

    Create reversearray.py

---
+++ Array in Python/reversearray.py
@@ -0,0 +1,11 @@
+#reverse() method in Python
+import array 
+
+numArr = array.array('i',[10,20,30,40,50])
+print("array items are:")
+print(numArr)
+
+
+numArr.reverse()
+print("\n array items after reversing the array: ")
+print(numArr)
