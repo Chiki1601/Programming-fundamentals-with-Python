82eb8fbc915b098d7fca738f15227ceef61102a3
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:57:23 +0530

    Create values().py

---
+++ Dictionary python programs/values().py
@@ -0,0 +1,18 @@
+#print only the data of the dictionary using values() method
+
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+print("Dicitonary data is given by: ")
+
+for x in bike.values():
+    print(x)
