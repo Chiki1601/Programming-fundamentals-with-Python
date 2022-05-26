cb432b8ea571136ba3e0afb426b4bd894622789b
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 19:00:55 +0530

    Create zero_field.py

---
+++ Multidimensional lists/zero_field.py
@@ -0,0 +1,6 @@
+#today we will see  how can we create multi dimentional list with zero field
+
+r = 4
+c = 3
+mList = [[0 for x in range(r) ]for x in range(c)]
+print("Multidimentional list is given by : ",mList)
