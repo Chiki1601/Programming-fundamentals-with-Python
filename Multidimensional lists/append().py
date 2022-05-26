8e192e8853c25a604ec69dda74818d58b8943ffc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 19:00:37 +0530

    Create append().py

---
+++ Multidimensional lists/append().py
@@ -0,0 +1,12 @@
+#append() method in multidimentional list
+
+
+
+mList = [[1,2,3,4,5],[1,2,3,4,5,6],[1,2,3,4,5,6,7]]
+
+print("Multidimentional list is given by : ",mList)
+
+
+mList.append([16,28,99,198])
+
+print("\n\nMultidimentional list is given by(After append) \n : ",mList)
