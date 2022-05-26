414079cad9cd93d1d9d1f12ae4eba9c5bb497a8a
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:55:37 +0530

    Create update().py

---
+++ Dictionary python programs/update().py
@@ -0,0 +1,27 @@
+#how can we add the item in dictionary  using update() method
+
+#how can we add the item in dicitonary
+#in Python 
+
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+print("Dicitonary data is given by: ")
+
+for x in bike.items():
+    print(x)
+    
+bike.update({"color":"Jet Black"})
+print("\n\nDicitonary data is given by(After adding the color of bike): ")
+
+for x in bike.items():
+    print(x)   
