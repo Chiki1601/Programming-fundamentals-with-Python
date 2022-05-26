2e905310c2501ef71b3df315722b568d7a4694dc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:55:04 +0530

    Create length of dictionary.py

---
+++ Dictionary python programs/length of dictionary.py
@@ -0,0 +1,21 @@
+#how can we find length of the dictionary
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+print("Dicitonary data is given by: ")
+
+for x in bike.items():
+    print(x)
+    
+    
+print("\n length of Bike dictionary: ",len(bike))
+    
