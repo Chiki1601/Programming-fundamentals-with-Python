c541fec81f60af37b3e6c85d2fce99d474aa95ca
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 23:01:54 +0530

    Create intarray.py

---
+++ Array in Python/intarray.py
@@ -0,0 +1,11 @@
+#array program in Python
+
+#int array demo
+import array
+# int array
+arrInt = array.array('i',[1,2,3,4,5])
+
+
+print("Integer Array: ")
+for x in range (len(arrInt)):
+    print(arrInt[x])
