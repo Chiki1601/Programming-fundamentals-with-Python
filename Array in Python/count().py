0b707c307ff6dd10ec670a63a288dc4679ba6a24
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 22:56:48 +0530

    Create count().py

---
+++ Array in Python/count().py
@@ -0,0 +1,9 @@
+#count() method in Python array
+
+
+import array as arr
+numArr =  arr.array('i',[10,20,30])
+print("Array is :",numArr)
+
+numArrRev = numArr[::-1]
+print("Array after reverse: ",numArrRev)
