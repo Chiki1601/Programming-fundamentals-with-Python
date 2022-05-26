b0e76ffaeb1d55dadae7aed0abdc28fc0bcbcfb6
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 22:58:59 +0530

    Create pop().py

---
+++ Array in Python/pop().py
@@ -0,0 +1,51 @@
+#pop() method in python array
+
+import array 
+            #1 = 20
+numArr = array.array('i',[10,20,30,40,50])
+print("array items are:")
+print(numArr)
+
+
+#deleting the item at index 1
+
+numArr.pop(1)
+print("\n array items after reversing the array: ")
+print(numArr)
+
+numArr.pop()
+print("\n array items after reversing the array: ")
+print(numArr)
+#pop() method in python array
+
+import array 
+            #1 = 20
+numArr = array.array('i',[10,20,30,40,50])
+print("array items are:")
+print(numArr)
+
+
+#deleting the item at index 1
+
+numArr.pop(1)
+print("\n array items after reversing the array: ")
+print(numArr)
+
+numArr.pop()
+print("\n array items after reversing the array: ")
+print(numArr)
+numArr.pop()
+print("\n array items after reversing the array: ")
+print(numArr)
+
+numArr.pop(0)
+print("\n array items after reversing the array: ")
+print(numArr)
+
+numArr.pop()
+print("\n array items after reversing the array: ")
+print(numArr)
+
+numArr.pop()
+print("\n array items after reversing the array: ")
+print(numArr)
