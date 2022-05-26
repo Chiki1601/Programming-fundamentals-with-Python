ecf5eb1ff409260105e1e8aa98cbb7a84f7d2169
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:59:10 +0530

    Create sort().py

---
+++ Multidimensional lists/sort().py
@@ -0,0 +1,13 @@
+#sort() method "Sort items of sublist " in multidimentional list
+
+mList = [[11,2,23],[2,14,9],[1,8,7]]
+
+
+print("Multidimentional list is given by : ",mList)
+
+mList[0].sort()
+mList[1].sort()
+mList[2].sort()
+
+
+print("\n\nMultidimentional list is given by(After sort) \n : ",mList)
