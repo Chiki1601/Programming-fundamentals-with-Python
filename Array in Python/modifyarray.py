c3a25efff32722b7225052f85515ec647f82f3a1
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 23:00:50 +0530

    Create modifyarray.py

---
+++ Array in Python/modifyarray.py
@@ -0,0 +1,10 @@
+import array as arr
+numArr = arr.array('i',[10,20,30,40,50,60,70,80,90,100])
+print("Array items are: ")
+print(numArr)
+
+#chainging index 2 value
+
+numArr[2] = 16
+print("\n array items after modyfying: ")
+print(numArr)
