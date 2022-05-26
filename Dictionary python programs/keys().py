2f02a37520eaf55fee1d733693b7e3eacc9216dc
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:56:47 +0530

    Create keys().py

---
+++ Dictionary python programs/keys().py
@@ -0,0 +1,18 @@
+#print only the data of the dictionary using keys() method
+
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+print("Dicitonary data is given by: ")
+
+for x in bike.keys():
+    print(x)
