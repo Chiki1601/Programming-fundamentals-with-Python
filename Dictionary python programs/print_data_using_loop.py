cc2f76f8cedb70a5880fec0d7c1e74ac76daf228
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-26 18:57:45 +0530

    Create print_data_using_loop.py

---
+++ Dictionary python programs/print_data_using_loop.py
@@ -0,0 +1,18 @@
+# only the data of the dictionary  using loop
+
+
+bike = {
+    
+    "brand": "Honda",
+
+"model" : "xyz",
+    
+    "year" : "2023",
+    "engine":"220cc",
+
+}
+
+print("Dicitonary data is given by: ")
+
+for x in bike:
+    print(bike[x])
