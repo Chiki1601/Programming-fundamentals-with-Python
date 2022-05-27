796cf1eba242c9d7bc868c8c1f6578509a067d87
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-27 14:16:22 +0530

    Create swap2.py

---
+++ General utility program/swap2.py
@@ -0,0 +1,10 @@
+#swap two numbers logic 2
+
+
+x =  int(input("Enter N number: "))
+y =  int(input("Enter N number: "))
+
+print("Before Swapping \n X = ",x,"Y = ",y)
+x,y = y,x
+
+print("After Swapping \n X = ",x,"Y = ",y)
