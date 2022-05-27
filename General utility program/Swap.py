b3911b0ab2667d75884925715a0cb37eb67b0743
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-27 14:16:11 +0530

    Create Swap.py

---
+++ General utility program/Swap.py
@@ -0,0 +1,12 @@
+#swap two numbers
+
+x =  int(input("Enter N number: "))
+y =  int(input("Enter N number: "))
+
+print("Before Swapping \n X = ",x,"Y = ",y)
+
+temp = x
+x = y
+y = temp
+
+print("After Swapping \n X = ",x,"Y = ",y)
